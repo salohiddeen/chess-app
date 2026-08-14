758c02728723b966300f34b185f739d0ed002ae0
salohiddeen <tarqymuhammadal@gmail.com>
2026-08-14 11:42:45 +0500

    initial successfully release

---
+++ apps/frontend/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import {
+  GitHubLogoIcon,
+} from '@radix-ui/react-icons';
+import { Link } from 'react-router-dom';
+
+export const Footer = () => {
+  return (
+    <footer className="mt-40 py-16 text-gray-400">
+      <div className="w-[96%] max-w-screen-lg mx-auto flex flex-col items-center justify-center">
+        <div>
+          <Link to={"/"}>Home</Link> |
+          <Link to={"/settings"}> Settings</Link> |
+          <Link to={"/login"}> Login</Link> |
+          <Link to={"/game/random"}> Play</Link>
+        </div>
+        <div>
+          <div className="flex gap-3 mt-4">
+            <a href="https://github.com/salohiddeen" target="_blank">
+              <GitHubLogoIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
